e97abbf3885a3baf71c7f1e88afdada2e3b34749
impupindersingh <pathaniarajput92@gmail.com>
2023-11-01 22:46:37 +0530

    Added topbar component's starter code

--- components/shared/TopBar.tsx
+++ components/shared/TopBar.tsx
@@ -1,5 +1,15 @@
+import Link from "next/link";
+import Image from "next/image";
+
 function TopBar() {
-  return <h1>TopBar</h1>;
+  return (
+    <nav className="topbar">
+      <Link href="/" className="flex items-center gap-4">
+        <Image src="/assets/logo.svg" alt="logo" width={28} height={28} />
+        <p className="text-heading3-bold text-light-1 max-xs:hidden">Threads</p>
+      </Link>
+    </nav>
+  );
 }
 
 export default TopBar;
