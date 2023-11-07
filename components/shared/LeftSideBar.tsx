3fb33cc584c33362eaed692399dbaecd4d5090d0
impupindersingh <pathaniarajput92@gmail.com>
2023-11-07 21:41:18 +0530

    Added isActive navigation buttons

--- components/shared/LeftSideBar.tsx
+++ components/shared/LeftSideBar.tsx
@@ -1,17 +1,36 @@
+"use client";
+
 import { sidebarLinks } from "@/constants";
 import Link from "next/link";
 import Image from "next/image";
+import { usePathname, useRouter } from "next/navigation";
 
 function LeftSideBar() {
+  const router = useRouter();
+  const pathname = usePathname();
   return (
     <section className="custom-scrollbar leftsidebar">
       <div className="flex w-full flex-1 flex-col gap-6 px-6">
-        {sidebarLinks.map((link) => (
-          <Link href={link.route} key={link.label} className="leftsidebar_link">
-            <Image src={link.imgURL} alt={link.label} width={24} height={24} />
-            <p className="text-light-1 max-lg:hidden">{link.label}</p>
-          </Link>
-        ))}
+        {sidebarLinks.map((link) => {
+          const isActive =
+            (pathname.includes(link.route) && link.route.length > 1) ||
+            pathname === link.route;
+          return (
+            <Link
+              href={link.route}
+              key={link.label}
+              className={`${isActive && "bg-primary-500"} leftsidebar_link`}
+            >
+              <Image
+                src={link.imgURL}
+                alt={link.label}
+                width={24}
+                height={24}
+              />
+              <p className="text-light-1 max-lg:hidden">{link.label}</p>
+            </Link>
+          );
+        })}
       </div>
     </section>
   );
