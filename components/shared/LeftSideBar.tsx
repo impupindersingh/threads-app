c40b4bd5fccc262d93b8118636479fa0c5a20598
impupindersingh <pathaniarajput92@gmail.com>
2023-11-07 21:49:15 +0530

    Added logout button to the left side

--- components/shared/LeftSideBar.tsx
+++ components/shared/LeftSideBar.tsx
@@ -4,6 +4,7 @@ import { sidebarLinks } from "@/constants";
 import Link from "next/link";
 import Image from "next/image";
 import { usePathname, useRouter } from "next/navigation";
+import { SignOutButton, SignedIn } from "@clerk/nextjs";
 
 function LeftSideBar() {
   const router = useRouter();
@@ -31,6 +32,21 @@ function LeftSideBar() {
             </Link>
           );
         })}
+      </div>
+      <div className="mt-10 px-6">
+        <SignedIn>
+          <SignOutButton signOutCallback={() => { router.push("/sign-in")}}>
+            <div className="flex cursor-pointer gap-4 p-4">
+              <Image
+                src="/assets/logout.svg"
+                alt="logout"
+                width={24}
+                height={24}
+              />
+              <p className="text-light-2 mx-lg:hidden">Logout</p>
+            </div>
+          </SignOutButton>
+        </SignedIn>
       </div>
     </section>
   );
