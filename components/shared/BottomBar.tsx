082ec9bd99eb3e7e83804568bc3ae99a8c9a8858
impupindersingh <pathaniarajput92@gmail.com>
2023-11-08 08:45:33 +0530

    Fixed overflowing with two words, make it to one

--- components/shared/BottomBar.tsx
+++ components/shared/BottomBar.tsx
@@ -26,7 +26,7 @@ function BottomBar() {
                 width={24}
                 height={24}
               />
-              <p className="text-subtle-medium text-light-1 max-sm:hidden">{link.label}</p>
+              <p className="text-subtle-medium text-light-1 max-sm:hidden">{link.label.split(/\s+/)[0]}</p>
             </Link>
           );
         })}
